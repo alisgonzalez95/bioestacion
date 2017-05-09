09bca3b470c5c9f64c330cc2ae27416b4fe99eb1
David Borja <dabor238@gmail.com>
2017-05-09 02:54:48 -0500

    cambios en formulario
    cambio de wufoo por API server

---
+++ index.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $('#gracias').hide();
+    $('#btnEnviar').click(function () {
+        var nombre = $('#txtNombre').val();
+        var mail = $('#txtMail').val();
+        var telefono = $('#txtTelefono').val();
+        var texto = $('#txt').val();
+
+        if (nombre != "" && mail != "" && telefono != "") {
+            var usuarioRequest = {
+                Nombre: nombre,
+                Email: mail,
+                Telefono: telefono,
+                TextArea: texto,
+                IdPrograma: 2
+            }
+            $.ajax({
+                type: 'POST',
+                url: 'http://www.resumitelo.com/api/usuarios/IngresoDatos',
+                dataType: 'json',
+                data: JSON.stringify(usuarioRequest),
+                contentType: 'application/json; charset=utf-8',
+                async: false,
+                success: function (data) {
+                    $('#formularioTexto').hide();
+                    $('#formularioCampos').hide();
+                    $('#gracias').show('slow');
+                },
+                error: function (xhr) {
+                    alert('Problemas al ingresar los datos. Intente nuevamente.');
+                }
+            });
+        } else {
+            alert('Por favor llene todos los campos');
+        }
+        
+    });
+    
+});
